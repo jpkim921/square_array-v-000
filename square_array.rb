50a412dd4ec62cbc988744f7915ae5ce731607eb
Jayon Kim <jpkim921@gmail.com>
2018-02-11 00:23:37 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -7,7 +7,23 @@ end
 
 
 def square_using_collect(array)
-  array.collect{ |x| x ** 2 }
 end
 
-p square_using_collect([2,4,6])
+
+
+
+# 
+# 
+# def square_array(array)
+#   # your code here
+#   ans_arr = []
+#   array.each { |x| ans_arr << x ** 2 }
+#   return ans_arr
+# end
+# 
+# 
+# def square_using_collect(array)
+#   array.collect{ |x| x ** 2 }
+# end
+# 
+# p square_using_collect([2,4,6])
